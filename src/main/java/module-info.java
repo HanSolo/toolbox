0c31b668df807385c9534848eab002b44cd26c60
Gerrit Grunwald <han.solo@mac.com>
2021-12-27 13:51:48 +0100

    cosmetics

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module eu.hansolo.toolbox {
 
     requires java.base;
 
-    exports eu.hansolo.toolbox;
-    exports eu.hansolo.toolbox.evt;
     exports eu.hansolo.toolbox.evt.type;
+    exports eu.hansolo.toolbox.evt;
     exports eu.hansolo.toolbox.properties;
     exports eu.hansolo.toolbox.tuples;
     exports eu.hansolo.toolbox.geom;
+    exports eu.hansolo.toolbox;
 }
